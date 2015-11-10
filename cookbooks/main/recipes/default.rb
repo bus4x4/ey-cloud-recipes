0a00a8e45784115e60b07ae904fb500af8ffda43
Francois van der Hoven <francois777@optusnet.com.au>
2015-11-10 11:52:55 +1000

    Baseline version

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -77,6 +77,8 @@
 #uncomment to use the solr recipe
 #include_recipe "solr"
 
+include_recipe "nginx_logrotate"
+
 #include_recipe "varnish_frontend"
 
 #uncomment to set environment variables in passenger or unicorn
